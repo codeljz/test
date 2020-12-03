4e2e58327093e4d80be4e99e83c4c31ce129036f
codeljz <75404808+codeljz@users.noreply.github.com>
2020-12-03 12:09:51 +0800

    上传文件
    第一次使用github上传文件

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- 导航栏 -->
+    <nav-bar></nav-bar>
+    <div class="nav-bar-white"></div>
+    <router-view></router-view>
+
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/nav/NavBar.vue'
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  },
+  created () {
+
+  }
+}
+</script>
+<style>
+@import "~styles/base.css";
+.nav-bar-white {
+  height: 30px;
+}
+</style>
